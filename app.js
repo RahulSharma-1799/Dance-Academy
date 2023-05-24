d01ac4eabe77120397d6bc0d2be0a79277e9f912
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-05-24 18:44:48 +0530

    make home page

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// EXPRESS SPECIFIC STUFF
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  const params = {};
+  res.status(200).render("index.pug", params);
+});
+
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});

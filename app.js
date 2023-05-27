1fb000163c21c8e3b9609f05490d37df5da4b125
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-05-27 18:29:05 +0530

    adding mongoDB database in contact

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0.o6evl2k.mongodb.net/contactdance",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+ );
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  address: String,
+  email: String,
+  desc: String
+});
+const Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static"));
@@ -11,14 +25,26 @@ app.use(express.urlencoded());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
+
+
+app.get('/', (req, res) => {
   const params = {};
-  res.status(200).render("home.pug", params);
+  res.status(200).render('home.pug', params);
 });
 
-app.get("/contact", (req, res) => {
+app.get('/contact', (req, res) => {
   const params = {};
-  res.status(200).render("contact.pug", params);
+  res.status(200).render('contact.pug');
+});
+
+app.post("/contact", (req, res) => {
+  var mydata = new Contact(req.body);
+  mydata.save().then(() => {
+    res.send("This item has been saved to the database");
+  }).catch(() => {
+    res.send("Item was not saved to the database");
+  })
+  // res.status(200).render("contact.pug");
 });
 
 // START THE SERVER

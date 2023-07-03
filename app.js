455226ef35263b7c31e28da7ec1209f607351ae4
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-07-03 15:57:40 +0530

    set environment variable

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express");
+require("dotenv").config();
 const path = require("path");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-mongoose.connect(
-  "mongodb+srv://root:root@cluster0.o6evl2k.mongodb.net/contactdance",
-  { useUnifiedTopology: true, useNewUrlParser: true }
- );
+mongoose.connect(process.env.MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 const contactSchema = new mongoose.Schema({
   name: String,
   phone: String,
   address: String,
   email: String,
-  desc: String
+  desc: String,
 });
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static"));
@@ -25,29 +26,30 @@ app.use(express.urlencoded());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const params = {};
-  res.status(200).render('home.pug', params);
+  res.status(200).render("home.pug", params);
 });
 
-app.get('/contact', (req, res) => {
+app.get("/contact", (req, res) => {
   const params = {};
-  res.status(200).render('contact.pug');
+  res.status(200).render("contact.pug");
 });
 
 app.post("/contact", (req, res) => {
   var mydata = new Contact(req.body);
-  mydata.save().then(() => {
-    res.send("This item has been saved to the database");
-  }).catch(() => {
-    res.send("Item was not saved to the database");
-  })
+  mydata
+    .save()
+    .then(() => {
+      res.send("This item has been saved to the database");
+    })
+    .catch(() => {
+      res.send("Item was not saved to the database");
+    });
   // res.status(200).render("contact.pug");
 });
 
 // START THE SERVER
-app.listen(port, ()=>{
-    console.log(`The application started successfully on port ${port}`);
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
 });
